125d8220fefdabd2e66376b0e1fb420ca5b9eae4
user2727 <gbarbariani@gmail.com>
2026-02-09 08:39:53 +0400

    add new project

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import { increment, decrement, incrementByAmount } from './counterSlice'
+
+export function Counter() {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <div style={{ display: 'flex', gap: '8px', justifyContent: 'center' }}>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      </div>
+    </div>
+  )
+}
